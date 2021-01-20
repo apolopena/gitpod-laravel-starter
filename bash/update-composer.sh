3e9527636a79ba410ddd8929fbe45a6a9e20eeb0
apolopena <3060702+apolopena@users.noreply.github.com>
2021-01-20 15:59:54 -0800

    bugfix

--- bash/update-composer.sh
+++ bash/update-composer.sh
@@ -15,11 +15,12 @@ then
 else
   php composer-setup.php --install-dir=/usr/bin --filename=composer
   RESULT=$?
-   if [$? -ne 0]
-   then
-    echo "ERROR $RESULT: Installing the latest version of composer, FAILED."
-   else
-    echo "SUCCESS: latest version of composer was installed"
+    if [$? -ne 0]
+    then
+      echo "ERROR $RESULT: Installing the latest version of composer, FAILED."
+    else
+      echo "SUCCESS: latest version of composer was installed"
+    fi
   rm composer-setup.php
 fi
 # END: update composer to the latest version
