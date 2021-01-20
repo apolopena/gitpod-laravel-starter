e189479caa2fe25735388109b45d63fe8ff5179c
apolopena <3060702+apolopena@users.noreply.github.com>
2021-01-20 15:57:30 -0800

    bugfix

--- bash/update-composer.sh
+++ bash/update-composer.sh
@@ -20,7 +20,6 @@ else
     echo "ERROR $RESULT: Installing the latest version of composer, FAILED."
    else
     echo "SUCCESS: latest version of composer was installed"
-   fi
   rm composer-setup.php
 fi
 # END: update composer to the latest version
