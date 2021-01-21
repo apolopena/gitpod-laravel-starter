82b8846852f67edb6ca012bd94bb714d5a29481f
apolopena <3060702+apolopena@users.noreply.github.com>
2021-01-20 16:03:41 -0800

    bugfix

--- bash/update-composer.sh
+++ bash/update-composer.sh
@@ -13,14 +13,10 @@ then
     >&2 echo 'ERROR: Invalid installer checksum - could not update composer'
     rm composer-setup.php
 else
-  php composer-setup.php --install-dir=/usr/bin --filename=composer
+  if ! php composer-setup.php --install-dir=/usr/bin --filename=composer; then
   RESULT=$?
-    if [$? -ne 0]
-    then
-      echo "ERROR $RESULT: Installing the latest version of composer, FAILED."
-    else
-      echo "SUCCESS: latest version of composer was installed"
-    fi
+  echo "ERROR $RESULT: Installing the latest version of composer, FAILED."
+  fi
   rm composer-setup.php
 fi
 # END: update composer to the latest version
