68ac62330a05605d4a24d6f130ce6f7a6be0a050
apolopena <3060702+apolopena@users.noreply.github.com>
2021-01-20 22:59:30 -0800

    scaffold project

--- bash/scaffold-project.sh
+++ bash/scaffold-project.sh
@@ -3,13 +3,13 @@ LOG='/var/log/workspace-image.log'
 
 echo "BEGIN: Scaffolding Laravel Project" | tee -a $LOG
 
-echo "Creating Laravel 8.23.x project in /temp-app ..." | tee -a $LOG
+echo "Creating Laravel 8.x project in /temp-app ..." | tee -a $LOG
 composer create-project --prefer-dist laravel/laravel test-app "8.*"
 ERRCODE=$?
 if [ $ERROCODE -ne 0 ]; then
   >&2 echo "  ERROR: failed to create Laravel 8.23.x project temp-app" | tee -a $LOG
 else
-  cd temp-app
+  cd ~/temp-app
   VER=`php artisan --version`
   echo "  SUCCESS: $VER project temp-app project created" | tee -a $LOG
 fi
