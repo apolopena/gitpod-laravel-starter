427c7902579e8e3a8e2088704e5d75f727fbd024
apolopena <3060702+apolopena@users.noreply.github.com>
2021-01-20 22:49:16 -0800

    making it work

--- bash/scaffold-project.sh
+++ bash/scaffold-project.sh
@@ -4,7 +4,7 @@ LOG='/var/log/workspace-image.log'
 echo "BEGIN: Scaffolding Laravel Project" | tee -a $LOG
 
 echo "Creating Laravel 8.23.x project in /temp-app ..." | tee -a $LOG
-composer create-project laravel/laravel:8.23.* temp-app
+composer create-project --prefer-dist laravel/laravel test-app "8.*"
 ERRCODE=$?
 if [ $ERROCODE -ne 0 ]; then
   >&2 echo "  ERROR: failed to create Laravel 8.23.x project temp-app" | tee -a $LOG
