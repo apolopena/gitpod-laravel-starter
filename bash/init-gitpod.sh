be183fe3d15a02cabc0edcbbb8229988846506be
apolopena <3060702+apolopena@users.noreply.github.com>
2021-01-21 00:29:00 -0800

    fixed scaffolding and output

--- bash/init-gitpod.sh
+++ bash/init-gitpod.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 printf "\033c"
-
-echo "Moving Laravel project from ~/temp-app to $GITPOD_REPO_ROOT ..."
+echo "Results of building the workspace image ➥"
+cat /var/log/workspace-image.log
+echo -e "\nMoving Laravel project from ~/temp-app to $GITPOD_REPO_ROOT ..."
 shopt -s dotglob
 mv -f ~/test-app/* $GITPOD_REPO_ROOT
-
-if [$? -ne 0]; then
+RESULT=$?
+if [ $? -ne 0 ]; then
   >&2 echo "ERROR: Failed to move Laravel project from ~/temp-app to $GITPOD_REPO_ROOT"
 else
   echo "SUCCESS: moved Laravel project from ~/temp-app to $GITPOD_REPO_ROOT"
 fi
-
 rmdir ~/test-app
-
-echo "Results of building the workspace image:"
-cat /var/log/workspace-image.log
-echo -e "\n\nIf the above results show success then make sure you add, commit and push the changes to your repo."
+echo "If the above results are successful then make sure you add, commit and push the changes to your git repository."
