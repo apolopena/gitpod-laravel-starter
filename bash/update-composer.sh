4dc5d7e5d40e7559115220881121707f791b911e
apolopena <3060702+apolopena@users.noreply.github.com>
2021-01-20 17:18:46 -0800

    script works well now and logs results

--- bash/update-composer.sh
+++ bash/update-composer.sh
@@ -12,6 +12,7 @@ if [ $COMP_PURGE -ne 0 ]; then
 else
   echo "  SUCCESS: purged existing version of composer." | tee -a $LOG
 fi
+
 echo "  Installing latest version of composer..."  | tee -a $LOG
 EXPECTED_CHECKSUM="$(wget -q -O - https://composer.github.io/installer.sig)"
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
@@ -32,5 +33,6 @@ else
   fi
   rm composer-setup.php
 fi
+
 echo "END: update composer" | tee -a $LOG
 # END: update composer to the latest version
