108548f29024a89cf86b2618f63ebd2be7e6a4ef
apolopena <3060702+apolopena@users.noreply.github.com>
2021-01-20 23:07:58 -0800

    tweaks

--- bash/scaffold-project.sh
+++ bash/scaffold-project.sh
@@ -9,7 +9,7 @@ ERRCODE=$?
 if [ $ERROCODE -ne 0 ]; then
   >&2 echo "  ERROR: failed to create Laravel 8.23.x project temp-app" | tee -a $LOG
 else
-  cd ~/temp-app
+  pwd | tee -a $LOG
   VER=`php artisan --version`
   echo "  SUCCESS: $VER project temp-app project created" | tee -a $LOG
 fi
