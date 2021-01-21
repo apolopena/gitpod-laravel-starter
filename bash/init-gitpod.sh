d5fa0f0e50e418beec941658af19126f5ab1a2b6
apolopena <3060702+apolopena@users.noreply.github.com>
2021-01-21 00:12:38 -0800

    refactor init and scaffolding

--- bash/init-gitpod.sh
+++ bash/init-gitpod.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 printf "\033c"
 
+echo "Moving Laravel project from ~/temp-app to $GITPOD_REPO_ROOT ..."
 shopt -s dotglob
 mv -f ~/test-app/* $GITPOD_REPO_ROOT
-rmdir ~/test-app
 
-sed -i "s|APP_URL=|APP_URL=${GITPOD_WORKSPACE_URL}|g" .env
-sed -i "s|APP_URL=https://|APP_URL=https://8000-|g" .env
+if [$? -ne 0]; then
+  >&2 echo "ERROR: Failed to move Laravel project from ~/temp-app to $GITPOD_REPO_ROOT"
+else
+  echo "SUCCESS: moved Laravel project from ~/temp-app to $GITPOD_REPO_ROOT"
+fi
+
+rmdir ~/test-app
 
 echo "Results of building the workspace image:"
 cat /var/log/workspace-image.log
-echo "\nMake sure you add, commit and push to your repo."
+echo -e "\n\nIf the above results show success then make sure you add, commit and push the changes to your repo."
--- bash/scaffold-project.sh
+++ bash/scaffold-project.sh
@@ -3,7 +3,7 @@ LOG='/var/log/workspace-image.log'
 
 echo "BEGIN: Scaffolding Laravel Project" | tee -a $LOG
 
-echo "  Creating Laravel 8.x project in /temp-app ..." | tee -a $LOG
+echo "  Creating Laravel 8.x project in ~/temp-app ..." | tee -a $LOG
 composer create-project --prefer-dist laravel/laravel test-app "8.*"
 ERRCODE=$?
 if [ $ERROCODE -ne 0 ]; then
@@ -12,7 +12,7 @@ else
   chown -R gitpod:gitpod /home/gitpod/test-app
   cd /home/gitpod/test-app
   VER=`php artisan --version`
-  echo "  SUCCESS: $VER project temp-app project created" | tee -a $LOG
+  echo "  SUCCESS: $VER project temp-app project created in ~/" | tee -a $LOG
 fi
 
 echo "END: Scaffolding Laravel Project" | tee -a $LOG
