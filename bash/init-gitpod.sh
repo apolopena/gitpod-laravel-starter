3766974c901107a285476261b99ae1123a5178fe
apolopena <3060702+apolopena@users.noreply.github.com>
2021-01-20 23:48:13 -0800

    sed APP_URL in .env

--- bash/init-gitpod.sh
+++ bash/init-gitpod.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+printf "\033c"
+
 shopt -s dotglob
 mv -f ~/test-app/* $GITPOD_REPO_ROOT
 rmdir ~/test-app
+
+sed -i "s|APP_URL=|APP_URL=${GITPOD_WORKSPACE_URL}|g" .env
+sed -i "s|APP_URL=https://|APP_URL=https://8000-|g" .env
+
+echo "Results of building the workspace image:"
 cat /var/log/workspace-image.log
+echo "\nMake sure you add, commit and push to your repo."
