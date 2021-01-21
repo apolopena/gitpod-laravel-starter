509fefb72a2d1d36f99a53f928ecf8bd7bd1d7ea
apolopena <3060702+apolopena@users.noreply.github.com>
2021-01-20 22:39:43 -0800

    fixing scaffolding

--- bash/scaffold-project.sh
+++ bash/scaffold-project.sh
@@ -9,12 +9,9 @@ ERRCODE=$?
 if [ $ERROCODE -ne 0 ]; then
   >&2 echo "  ERROR: failed to create Laravel 8.23.x project temp-app" | tee -a $LOG
 else
+  cd temp-app
   VER=`php artisan --version`
   echo "  SUCCESS: $VER project temp-app project created" | tee -a $LOG
 fi
-
-echo "Moving temp-app to repo root..."
-mv -f 
-
 
 echo "END: Scaffolding Laravel Project" | tee -a $LOG
