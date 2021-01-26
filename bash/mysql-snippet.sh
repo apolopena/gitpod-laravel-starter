409a4d0765e3e449ccfe8f59ad53d6cb37b1734a
apolopena <3060702+apolopena@users.noreply.github.com>
2021-01-25 20:36:09 -0800

    bugfix

--- bash/mysql-snippet.sh
+++ bash/mysql-snippet.sh
@@ -1,8 +1,8 @@
 if [ $MYSQL_INIT -ne 0 ]; then
-  . /etc/mysql/spinner.sh && start_spinner "Initializing MySql, this may take a moment." >> ~/.bashrc
+  . /etc/mysql/spinner.sh && start_spinner "Initializing MySql, this may take a moment."
 fi
-echo "/etc/mysql/mysql-bashrc-launch.sh" >> ~/.bashrc
+/etc/mysql/mysql-bashrc-launch.sh
 if [ $MYSQL_INIT -ne 0 ]; then
-  echo "stop_spinner $?" >> ~/.bashrc
-  export MYSQL_INIT=0
+  stop_spinner $?
+  MYSQL_INIT=0
 fi
