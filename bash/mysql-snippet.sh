3b6b21fc81833716b5ef4b67e1fae2da8f192279
apolopena <3060702+apolopena@users.noreply.github.com>
2021-01-26 12:48:51 -0800

    timing and spinner work

--- bash/mysql-snippet.sh
+++ bash/mysql-snippet.sh
@@ -2,7 +2,7 @@ if [[ ! -v MYSQL_INIT ]]; then
     #echo "MYSQL_INIT is not set"
     /etc/mysql/mysql-bashrc-launch.sh
 elif [[ -z "$MYSQL_INIT" ]]; then
-    #echo "MYSQL_INIT is set to the empty string"
+    echo "MYSQL_INIT is set to the empty string"
 else
     #echo "MYSQL_INIT has the value: $MYSQL_INIT"
     . /etc/mysql/spinner.sh && start_spinner "Initializing MySql, this may take a moment."
