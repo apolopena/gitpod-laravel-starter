b42c5e2633cdfb9babaaa3ccd27016026e920ee6
apolopena <3060702+apolopena@users.noreply.github.com>
2021-01-25 22:02:57 -0800

    bugfix

--- bash/mysql-snippet.sh
+++ bash/mysql-snippet.sh
@@ -7,13 +7,6 @@ else
     #echo "MYSQL_INIT has the value: $MYSQL_INIT"
     . /etc/mysql/spinner.sh && start_spinner "Initializing MySql, this may take a moment."
     /etc/mysql/mysql-bashrc-launch.sh
-fi
-
-if [[ ! -v MYSQL_INIT ]]; then
-    #echo "MYSQL_INIT is not set"
-elif [[ -z "$MYSQL_INIT" ]]; then
-    echo "MYSQL_INIT is set to the empty string"
-else
     stop_spinner
     unset MYSQL_INIT
 fi
