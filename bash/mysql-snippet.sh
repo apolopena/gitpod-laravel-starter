f0896a4228e41520f39a82158037e93290bcba4e
apolopena <3060702+apolopena@users.noreply.github.com>
2021-01-26 20:55:55 -0800

    previous commit did not do the trick, now removing spinner references

--- bash/mysql-snippet.sh
+++ bash/mysql-snippet.sh
@@ -6,8 +6,8 @@ elif [[ -z "$MYSQL_INIT" ]]; then
     echo "MYSQL_INIT is set to the empty string"
 else
     #echo "MYSQL_INIT has the value: $MYSQL_INIT"
-    . /etc/mysql/spinner.sh && start_spinner "Initializing MySql, this may take a moment."
+  #  . /etc/mysql/spinner.sh && start_spinner "Initializing MySql, this may take a moment."
     /etc/mysql/mysql-bashrc-launch.sh
-    stop_spinner $?
+   # stop_spinner $?
     unset MYSQL_INIT
 fi
