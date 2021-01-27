afe53f43903a76b9bf0e29afb2c4ed371ca77049
apolopena <3060702+apolopena@users.noreply.github.com>
2021-01-26 21:17:10 -0800

    force build

--- .gitpod.Dockerfile
+++ .gitpod.Dockerfile
@@ -38,7 +38,7 @@ RUN sudo bash -c ". /tmp/update-composer.sh" && rm /tmp/update-composer.sh
 
 # gitpod trick to bypass the docker caching mechanism for all lines below this one
 # just increment the value each time you want to bypass the cache system
-ENV INVALIDATE_CACHE=94
+ENV INVALIDATE_CACHE=95
 
 COPY --chown=gitpod:gitpod bash/scaffold-project.sh /tmp
 RUN sudo bash -c ". /tmp/scaffold-project.sh" && rm /tmp/scaffold-project.sh
--- bash/mysql-snippet.sh
+++ bash/mysql-snippet.sh
@@ -5,9 +5,5 @@ if [[ ! -v MYSQL_INIT ]]; then
 elif [[ -z "$MYSQL_INIT" ]]; then
     echo "MYSQL_INIT is set to the empty string"
 else
-    #echo "MYSQL_INIT has the value: $MYSQL_INIT"
-  #  . /etc/mysql/spinner.sh && start_spinner "Initializing MySql, this may take a moment."
-    /etc/mysql/mysql-bashrc-launch.sh
-   # stop_spinner $?
-    unset MYSQL_INIT
+    . /etc/mysql/spinner.sh && start_spinner "Initializing MySql, this may take a few moments." && /etc/mysql/mysql-bashrc-launch.sh && stop_spinner $? && unset MYSQL_INIT
 fi
