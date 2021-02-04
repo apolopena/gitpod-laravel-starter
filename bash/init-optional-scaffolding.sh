4789a466f3a614943c1993eb83603314ca0093ce
apolopena <3060702+apolopena@users.noreply.github.com>
2021-02-03 23:14:57 -0800

    🐛 FIX: feat 20: set specific react and react-dom version

--- bash/init-optional-scaffolding.sh
+++ bash/init-optional-scaffolding.sh
@@ -4,17 +4,17 @@
 # Description:
 # Installs various packages according to the configuration set in starter.ini
 
-# Parse starter.ini
+# BEGIN: Parse starter.ini
 install_react=$(. /tmp/utils.sh parse_ini_value starter.ini react install)
 install_bootstrap=$(. /tmp/utils.sh parse_ini_value starter.ini bootstrap install)
-
-# Install Laravel ui if needed
+# END: Parse starter.ini
+# BEGIN: Install Laravel ui if needed
 if [[ $install_react == 1 || $install_bootstrap  == 1 ]]; then
   echo "Optional installations that require laravel/ui scaffolding were found."
   echo "Installing laravel/ui scaffolding..."
-  composer require laravel/ui
+  composer require laravel/ui:^3.2.0
   err_code=$?
-  if [ $err_code -eq 0 ]; then
+  if [ $err_code == 0 ]; then
     echo "SUCCESS: laravel/ui scaffolding installed"
     # genrate yarn.lock without installing anything
     # this is a npm workaround since yarn does not have this feature
@@ -26,9 +26,9 @@ if [[ $install_react == 1 || $install_bootstrap  == 1 ]]; then
     >&2 echo "ERROR $err_code: There was a problem installing laravel/ui"
   fi
 fi
-
+# END: Install Laravel ui if needed
 # BEGIN: Optional react and react-dom install
-if [ $install_react -eq 1 ]; then
+if [ $install_react == 1 ]; then
   version=$(. /tmp/utils.sh parse_ini_value starter.ini react version)
   auth=$(. /tmp/utils.sh parse_ini_value starter.ini react auth)
 
@@ -48,11 +48,27 @@ if [ $install_react -eq 1 ]; then
     echo "SUCCESS: React and React DOM$version_msg$auth_msg has been installed"
     echo "Compiling fresh scaffolding and running Laravel Mix"
     yarn install && yarn run dev && sleep 1 && yarn run dev
+    echo "setting react and react-dom to$version_msg"
+    # TODO:  validate semver and valid version for the package so users cant pass in junk
+    yarn upgrade react@$version react-dom@$version
   else
     >&2 echo "ERROR $err_code: There was a problem installing React/React DOM$version_msg$auth_msg"
   fi
-# END: Optional react and react-dom install
 fi
+# END: Optional react and react-dom install
+# BEGIN: Optional vue install
+# END: Optional vue install
+# BEGIN: Optional bootstrap install
+# END: Optional bootstrap install
+
+# BEGIN: set react and react-dom to specific version
+
+# END: set react and react-dom to specific version
+# BEGIN: set vue to specific version
+# END: set vue to specific version
+# BEGIN: set bootstrap to specific version
+# END: set bootstrap to specific version
+
 
 # Finally install everything else
 echo "Installing remaining dependencies"
