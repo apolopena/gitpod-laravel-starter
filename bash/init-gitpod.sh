a6b0655182595c8ae5afdeba44ecfa16fa96c8a5
apolopena <3060702+apolopena@users.noreply.github.com>
2021-02-05 11:27:41 -0800

    🐛 FIX: typo

--- bash/init-gitpod.sh
+++ bash/init-gitpod.sh
@@ -4,13 +4,13 @@ log_path=/var/log/workspace-init.log
 
 log () {
   local cmd
-  [[ "$2" == '-e' || "$2" == '--error'] && cmd='>&2 echo -e'
+  [[ "$2" == '-e' || "$2" == '--error' ]] && cmd='>&2 echo -e'
   eval $cmd "$1" | tee -a $log_path
 }
 
 log_silent () {
   local cmd
-  [[ "$2" == '-e' || "$2" == '--error'] && cmd='>&2 echo -e'
+  [[ "$2" == '-e' || "$2" == '--error' ]] && cmd='>&2 echo -e'
   eval $cmd "$1" >> $log_path
 }
 # Load spinner
