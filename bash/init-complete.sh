501877b437d19df57f54c901d68e8ad485d9c54a
apolopena <3060702+apolopena@users.noreply.github.com>
2021-02-05 16:53:46 -0800

    🐛 FIX: silly error in https://github.com/apolopena/gitpod-laravel8-starter/issues/24

--- bash/init-complete.sh
+++ bash/init-complete.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-$workspace_log='/var/log/workspace-image.log'
-$init_log='/var/log/workspace-init.log'
+workspace_log='/var/log/workspace-image.log'
+init_log='/var/log/workspace-init.log'
 echo -e "\nALL DONE\n"
 echo "Results of building the workspace image $workspace_log ➥"
-cat /var/log/workspace-image.log
+cat $workspace_log
 echo ''
-echo "Results of the gitpod initialization  ➥"
-cat /var/log/workspace-init.log
-echo -e "/nIf everything looks good in the above results then push the newly created project files to your git repo and get started customizing your project 🚀"
+echo "Results of the gitpod initialization $init_log ➥"
+cat $init_log
+echo -e "\nIf everything looks good in the above results then push the newly created project files to your git repo and get started customizing your project 🚀"
