4c0becbb35d32bc2ccbf595d0aa30c6e3473cedb
apolopena <3060702+apolopena@users.noreply.github.com>
2021-02-05 16:31:47 -0800

    🐛 FIX: https://github.com/apolopena/gitpod-laravel8-starter/issues/24

--- bash/init-complete.sh
+++ bash/init-complete.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-$workspace_log="/var/log/workspace-image.log"
-$init_log="/var/log/workspace-init.log"
+$workspace_log='/var/log/workspace-image.log'
+$init_log='/var/log/workspace-init.log'
 echo -e "\nALL DONE\n"
 sleep 3
 echo "Results of building the workspace image $workspace_log ➥"
