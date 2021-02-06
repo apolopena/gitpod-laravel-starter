1f7579a5bf1805ced158f476b9f08d4a452463ba
apolopena <3060702+apolopena@users.noreply.github.com>
2021-02-05 16:56:54 -0800

    📖 DOC: console messages

--- bash/init-complete.sh
+++ bash/init-complete.sh
@@ -3,9 +3,9 @@
 workspace_log='/var/log/workspace-image.log'
 init_log='/var/log/workspace-init.log'
 echo -e "\nALL DONE\n"
-echo "Results of building the workspace image $workspace_log ➥"
+echo "Results of building the workspace image $workspace_log ➥\n"
 cat $workspace_log
 echo ''
 echo "Results of the gitpod initialization $init_log ➥"
 cat $init_log
-echo -e "\nIf everything looks good in the above results then push the newly created project files to your git repo and get started customizing your project 🚀"
+echo -e "\nIf everything looks good in the above results then push the newly created project files to your git repo and get started coding your project 🚀"
