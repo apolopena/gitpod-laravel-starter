46963ec1e6db546c666d9268d177cde0d01e5f65
apolopena <3060702+apolopena@users.noreply.github.com>
2021-02-05 23:51:39 -0800

    🐛 FIX: adjust colors

--- bash/init-complete.sh
+++ bash/init-complete.sh
@@ -2,13 +2,13 @@
 
 workspace_log='/var/log/workspace-image.log'
 init_log='/var/log/workspace-init.log'
-echo -e "\n\e[38;5;177mSUMMARY\e[0m\n"
-echo -en "\e[38;5;177mResults of building the workspace image $workspace_log ➥\n"
+echo -e "\n\e[38;5;171mSUMMARY\e[0m\n"
+echo -en "\e[38;5;183mResults of building the workspace image $workspace_log ➥\n"
 cat $workspace_log
 echo -en "\e[0m"
 echo ''
-echo -en "\e[38;5;189mResults of the gitpod initialization $init_log ➥"
+echo -en "\e[38;5;39mResults of the gitpod initialization $init_log ➥"
 cat $init_log
 echo -en "\e[0m"
-echo -e "\n\e[38;5;177mALL DONE\e[0m\n"
+echo -e "\n\e[38;5;171mALL DONE\e[0m\n"
 echo "If everything looks good in the above results then push the newly created project files to your git repo and get started coding your project 🚀\n"
