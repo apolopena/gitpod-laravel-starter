5c890fb8e293f5b6bedc117bd444fd6070cfbae4
apolopena <3060702+apolopena@users.noreply.github.com>
2021-02-06 00:26:36 -0800

    🐛 FIX: add last of the flare for now

--- bash/init-complete.sh
+++ bash/init-complete.sh
@@ -2,7 +2,7 @@
 
 workspace_log='/var/log/workspace-image.log'
 init_log='/var/log/workspace-init.log'
-echo -en "\n\e[38;5;171mSUMMARY\e[0m\n"
+echo -en "\n\e[38;5;171mSUMMARY 📃\e[0m\n"
 echo -en "\e[38;5;194mResults of building the workspace image\e[0m \e[38;5;34m$workspace_log\e[0m ➥\n\e[38;5;183m"
 cat $workspace_log
 echo -en "\e[0m"
@@ -11,4 +11,4 @@ echo -en "\e[38;5;194mResults of the gitpod initialization\e[0m \e[38;5;34m$init
 cat $init_log
 echo -en "\e[0m"
 echo -en "\n\e[38;5;171mALL DONE 🚀\e[0m\n"
-echo -en "\e[38;5;194mIf everything looks good in the above results then push the newly created project files to your git repo and get started coding your project\e[0m"
+echo -en "\e[38;5;194mIf everything looks good in the above results then \npush the newly created project files to your git repo and get started coding your project\e[0m"
