4dd195f3421660a4adf1619e77dedff72dfcc489
apolopena <3060702+apolopena@users.noreply.github.com>
2021-03-09 11:18:43 -0800

    🐛 FIX: use native rsync progress option

--- bash/init-gitpod.sh
+++ bash/init-gitpod.sh
@@ -34,7 +34,7 @@ if [ ! -d "$GITPOD_REPO_ROOT/vendor" ]; then
   # TODO: replace spinner with a real progress bar for coreutils
   log_silent "$msg..." && start_spinner "$msg..."
   shopt -s dotglob
-  rsync -rlptgoD --ignore-existing --info=name0 --no-i-r ~/test-app/ $GITPOD_REPO_ROOT | xargs -L1 printf "\33[2K\r%s"
+  rsync -rlptgoD --ignore-existing --info=progress2 --no-i-r ~/test-app/ $GITPOD_REPO_ROOT | xargs -L1 printf "\33[2K\r%s"
   err_code=$?
   if [ $err_code != 0 ]; then
     stop_spinner $err_code
