6a0441cb4bf68d221c255d419305abad59575f01
apolopena <3060702+apolopena@users.noreply.github.com>
2021-03-10 08:52:27 -0800

    🐛 FIX: refactor to save calls to yarn install

--- bash/init-optional-scaffolding.sh
+++ bash/init-optional-scaffolding.sh
@@ -45,8 +45,8 @@ if [[ $install_react == 1 || $install_bootstrap  == 1 ]]; then
     err_code=$?
     if [ $err_code == 0 ]; then
       log "SUCCESS: laravel/ui scaffolding installed"
-      log "Compiling fresh scaffolding and running Laravel Mix"
-      yarn install && yarn run dev
+      #log "Compiling fresh scaffolding and running Laravel Mix"
+      #yarn install && yarn run dev
     else
       log "ERROR $err_code: There was a problem installing laravel/ui" -e
     fi
@@ -73,7 +73,7 @@ if [ "$install_react" == 1 ]; then
     fi
     err_code=$?
     if [ $err_code == 0 ]; then
-      log "SUCCESS: React and React DOM$version_msg$auth_msg have been installed"
+      log "SUCCESS: React and React DOM$version_msg$auth_msg has been installed"
       if [ $install_only_frontend_scaffolding == 1 ]; then
         log "Compiling fresh scaffolding and running Laravel Mix"
         yarn install && yarn run dev && sleep 1 && yarn run dev
