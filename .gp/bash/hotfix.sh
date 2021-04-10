4aea392964b36f929453b08424a9de49b3672668
apolopena <3060702+apolopena@users.noreply.github.com>
2021-04-09 17:46:22 -0700

    ➕ ADD: hotfix message

---
+++ .gp/bash/hotfix.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Hotfix for https://github.com/apolopena/gitpod-laravel8-starter/issues/19
+echo "Gitpod was not able to open the preview. You may open (or refresh) the preview by running the alias: op"
