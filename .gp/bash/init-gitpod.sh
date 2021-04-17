ca8850a8c388c1475e15c7f242edfddbc6288870
apolopena <3060702+apolopena@users.noreply.github.com>
2021-04-17 13:40:52 -0700

    🐛 FIX: update npm to ^7

--- .gp/bash/init-gitpod.sh
+++ .gp/bash/init-gitpod.sh
@@ -20,19 +20,20 @@ stop_spinner $?
 
 # BEGIN: Update npm if needed
 target_npm_ver='^7'
+min_target_npm_ver='7.7.5'
 current_npm_ver=$(npm -v)
-update_npm=$(bash .gp/bash/utils.sh comp_ver_lt "$current_npm_ver" "$target_npm_ver")
+update_npm=$(bash .gp/bash/utils.sh comp_ver_lt "$current_npm_ver" "$min_target_npm_ver")
 if [[ $update_npm == 1 ]]; then
-  msg="Updating npm from $current_npm_ver to $target_npm_ver"
-  log_silent "$msg" && start_spinner "$msg"
+  msg="Updating npm from $current_npm_ver to "
+  log_silent "$msg $target_npm_ver" && start_spinner "$msg $target_npm_ver"
   npm install -g "npm@$target_npm_ver" &>/dev/null
   err_code=$?
   if [ $err_code != 0 ]; then
     stop_spinner $err_code
-    log -e "ERROR $?: $msg"
+    log -e "ERROR $?: $msg a version >= $min_target_npm_ver"
   else
     stop_spinner $err_code
-    log_silent "SUCCESS: $msg"
+    log_silent "SUCCESS: $msg $(npm -v)"
   fi
 fi
 # END: Update npm if needed
