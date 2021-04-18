808aa012fcba9c365b683c6a7be02344d3d1c8e4
apolopena <3060702+apolopena@users.noreply.github.com>
2021-04-18 11:08:58 -0700

    🐛 FIX: https://github.com/apolopena/gitpod-laravel-starter/issues/111

--- .gp/snippets/browser-functions.sh
+++ .gp/snippets/browser-functions.sh
@@ -19,30 +19,38 @@ server_port() {
 
 debug_on() {
   local refresh stype port
-  local usage='Usage: debug_on server-type [no-refresh]'
   local err='Error: debug_on():'
+  local msg='Activated debug session for the'
+  local default_server=
+  default_server=$(bash "$GITPOD_REPO_ROOT"/.gp/bash/utils.sh parse_ini_value starter.ini development default_server)
   refresh=$(echo "$2" | tr '[:upper:]' '[:lower:]')
   stype=$(echo "$1" | tr '[:upper:]' '[:lower:]')
-  # Error handling
-  [ -z "$stype" ] && echo "$err must have at least one argument" && echo "$usage" && return
+  # Error handling and value assignment
+  [[ $stype == 'no-refresh' ]] && echo "$msg default server: $default_server" && stype=$default_server && refresh='no-refresh'
+  [[ -z $stype ]] && echo "$msg default server: $default_server" && stype=$default_server
+  [[ -z $default_server ]] && echo "$err could not determine default server. Try passing in a valid server type." && return
   [[ $stype != 'php' && $stype != 'apache' && $stype != 'nginx' ]] \
   && echo -e "$err Invalid default server type $stype.\nCheck starter.ini for supported values" && return
-  [ "$refresh" != '' ] && [ "$refresh" != 'no-refresh' ] && echo "$err invalid refresh argument: $refresh" && return
+  [[ $refresh != '' ]] && [ "$refresh" != 'no-refresh' ] && echo "$err invalid refresh argument: $refresh" && return
   # Set port
   if ! port=$(server_port "$stype"); then echo "Internal $err server_port() defaulted to $port"; fi
   # Start debug session
-  [ "$refresh" == 'no-refresh' ] && gp preview "$(gp url "$port")?XDEBUG_SESSION_START=$stype" && return
+  [[ $refresh == 'no-refresh' ]] && gp preview "$(gp url "$port")?XDEBUG_SESSION_START=$stype" && return
   gp preview "$(gp url "$port")?XDEBUG_SESSION_START=$stype" && sleep 1 && gp preview "$(gp url "$port")"
 }
 
 debug_off() {
   local refresh stype port
-  local usage='Usage: debug_off server-type [no-refresh]'
   local err='Error: debug_off():'
+  local msg='Deactivated debug session for the'
+  local default_server=
+  default_server=$(bash "$GITPOD_REPO_ROOT"/.gp/bash/utils.sh parse_ini_value starter.ini development default_server)
   refresh=$(echo "$2" | tr '[:upper:]' '[:lower:]')
   stype=$(echo "$1" | tr '[:upper:]' '[:lower:]')
-  # Error handling
-  [ -z "$stype" ] && echo "$err must have at least one argument" && echo "$usage" && return
+  # Error handling and value assignment
+  [[ $stype == 'no-refresh' ]] && echo "$msg default server: $default_server" && stype=$default_server && refresh='no-refresh'
+  [[ -z $stype ]] && echo "$msg default server: $default_server" && stype=$default_server
+  [[ -z $default_server ]] && echo "$err could not determine default server. Try passing in a valid server type." && return
   [[ $stype != 'php' && $stype != 'apache' && $stype != 'nginx' ]] \
   && echo -e "$err Invalid default server type $stype.\nCheck starter.ini for supported values" && return
   [ "$refresh" != '' ] && [ "$refresh" != 'no-refresh' ] && echo "$err invalid refresh argument: $refresh" && return
