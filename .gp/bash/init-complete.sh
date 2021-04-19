7b87df996f28483a98e6be48acb7f1c319bdd998
apolopena <3060702+apolopena@users.noreply.github.com>
2021-04-18 17:15:08 -0700

    ⚰️ REMOVE: extraneous message

--- .gp/bash/init-complete.sh
+++ .gp/bash/init-complete.sh
@@ -16,7 +16,6 @@ allow_mixed_web=$(bash .gp/bash/utils.sh parse_ini_value starter.ini laravel all
 
 # Summarize results
 bash .gp/bash/helpers.sh show_first_run_summary
-echo -e "\e[38;5;194mIf everything looks good in the above results then push any new\nproject files to your git repository. Your project is ready to code.\e[0m"
 
 # Persist the workspace-init.log since the .gitpod.Dockerfile will wipe it out and it wont come back after the first run
 bash .gp/bash/helpers.sh persist_file /var/log/workspace-init.log
