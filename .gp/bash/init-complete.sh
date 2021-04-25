e14297fc232bffac101052187c4f91031e487ac9
apolopena <3060702+apolopena@users.noreply.github.com>
2021-04-25 13:30:44 -0700

    🐛 FIX: missing logger

--- .gp/bash/init-complete.sh
+++ .gp/bash/init-complete.sh
@@ -10,13 +10,16 @@
 # Notes:
 # Always call this file last from the 'init' command in .gitpod.yml
 
+# Load logger
+. .gp/bash/workspace-init-logger.sh
+
 # Inject routes/web.php
 allow_mixed_web=$(bash .gp/bash/utils.sh parse_ini_value starter.ini laravel allow_mixed_web)
 [[ $allow_mixed_web != 0 ]] && bash .gp/bash/directives/allow-mixed-web.sh
 
 # Cleanup
 if rm -rf /home/gitpod/laravel-starter;then
-  log_silent "CLEANUP SUCCESS: removed ~/laravel-starter"
+  log "CLEANUP SUCCESS: removed ~/laravel-starter"
 fi
 
 # Summarize results
