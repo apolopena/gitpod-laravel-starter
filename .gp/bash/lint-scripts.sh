85d0be0c33ce33d07fa05f9b2d010315ad60714e
apolopena <penavilla@gmail.com>
2021-04-28 22:54:01 +0000

    ♻️ REFACTOR: to pass shellcheck

--- .gp/bash/lint-scripts.sh
+++ .gp/bash/lint-scripts.sh
@@ -25,7 +25,7 @@ script_total() {
 }
 
 main() {
-  local total result skip skip_msg
+  local total result exclude exclude_msg
   if [[ $1 == '-V' || $1 == '--verbose' ]]; then
     echo -e "\e[38;5;87mRunning all starter scripts through shellcheck\e[0m"
     echo -ne "\e[38;5;45m"
@@ -34,17 +34,17 @@ main() {
   total=$(script_total)
   (( total == 0 )) && echo -e "\e[38;5;124mInternal Error:\e[0m no scripts found to lint" && exit 1
   if [[ ! -e $(pwd)/.gp ]]; then
-    skip_msg="\e[38;5;208mWARNING: You did not lint the starter scripts from the project root so shellcheck error \e[0mSC1091\e[38;5;208m was excluded.\e[0m"
-    skip='-e SC1091'
+    exclude_msg="\e[38;5;208mWARNING: You did not lint the starter scripts from the project root so the shellcheck error \e[0mSC1091\e[38;5;208m was excluded.\e[0m"
+    exclude=(-e SC1091)
   fi
-  result=$(find "$(path)" -type d \( -name node_modules \) -prune -false -o -name "*.sh" -exec shellcheck -x -P "$(path)" $skip {} \; | tee >(wc -l))
+  result=$(find "$(path)" -type d \( -name node_modules \) -prune -false -o -name "*.sh" -exec shellcheck -x -P "$(path)" "${exclude[@]}" {} \; | tee >(wc -l))
   if [[ $result == 0 ]]; then
-    [[ -n $skip_msg ]] && echo -e "$skip_msg"
+    [[ -n $exclude_msg ]] && echo -e "$exclude_msg"
     echo -e "\e[38;5;76mSUCCESS:\e[0m \e[38;5;40mall \e[0;36m$total\e[0m \e[38;5;40mscripts in the system passed the shellcheck.\e[0m"
     exit
   fi
-  find "$(path)" -type d \( -name node_modules \) -prune -false -o -name "*.sh" -exec shellcheck -x -P "$(path)" $skip {} \;
-  [[ -n $skip_msg ]] && echo -e "$skip_msg"
+  find "$(path)" -type d \( -name node_modules \) -prune -false -o -name "*.sh" -exec shellcheck -x -P "$(path)" "${exclude[@]}" {} \;
+  [[ -n $exclude_msg ]] && echo -e "$exclude_msg"
 }
 
 main "$1"
