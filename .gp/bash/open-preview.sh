f96c649b3b04fb0d4910fdc06da568de156aa66f
apolopena <penavilla@gmail.com>
2021-04-28 19:58:24 +0000

    🐛 FIX: clearer log message

--- .gp/bash/open-preview.sh
+++ .gp/bash/open-preview.sh
@@ -9,7 +9,7 @@
 # either to the web root if no argument is passed in
 # or to the specified path segment if $1 is passed in
 [[ $(pwd) != "$GITPOD_REPO_ROOT" ]] &&
-echo -e "\e[1;31mThis command can only be run from the project root:\e[0m $GITPOD_REPO_ROOT" &&
+echo -e "\e[1;31mThis command can only be run from the project root of a gitpod workspace:\e[0m $GITPOD_REPO_ROOT" &&
 
 exit 1
 # Load spinner
