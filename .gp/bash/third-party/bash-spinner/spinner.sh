b7b3a5f20d0526fdd9432cdb24918c20b9c26d90
apolopena <penavilla@gmail.com>
2021-04-28 06:42:03 +0000

    👌 IMPROVE: support custom DONE and FAILED messages ()

--- .gp/bash/third-party/bash-spinner/spinner.sh
+++ .gp/bash/third-party/bash-spinner/spinner.sh
@@ -69,14 +69,18 @@ function _spinner() {
 
             kill $3 > /dev/null 2>&1
 
-            # inform the user uppon success or failure
-            echo -en "\b["
-            if [[ $2 -eq 0 ]]; then
-                echo -en "${green}${on_success}${nc}"
+            # inform the user upon success or failure
+            if [[ -z $4 ]]; then
+              echo -en "\b["
+              if [[ $2 -eq 0 ]]; then
+                  echo -en "${green}${on_success}${nc}"
+              else
+                  echo -en "${red}${on_fail}${nc}"
+              fi
+              echo -e "]"
             else
-                echo -en "${red}${on_fail}${nc}"
+              echo -e "$4"
             fi
-            echo -e "]"
             ;;
         *)
             echo "invalid argument, try {start/stop}"
@@ -95,6 +99,7 @@ function start_spinner {
 
 function stop_spinner {
     # $1 : command exit status
-    _spinner "stop" $1 $_sp_pid
+    # $2 : optional message to replace [DONE] results on success
+    _spinner "stop" $1 $_sp_pid "$2"
     unset _sp_pid
 }
