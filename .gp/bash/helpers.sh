54aa44d0aea71dab836b20d94415faa9b53685f9
apolopena <3060702+apolopena@users.noreply.github.com>
2022-01-21 12:33:07 -0800

    🐛 FIX: bad tabs in php-fpm.conf generation

--- .gp/bash/helpers.sh
+++ .gp/bash/helpers.sh
@@ -379,20 +379,20 @@ laravel_ui_version() {
 php_fpm_conf() {
   [[ -z $1 || -z $2 ]] && 2>&1 echo "  ERROR: utils.sh --> php_fpm_conf(): Bad args. Script aborted" && exit 1
   echo "\
-  [global]
-  pid = /tmp/php$1-fpm.pid
-  error_log = /tmp/php$1-fpm.log
+[global]
+pid = /tmp/php$1-fpm.pid
+error_log = /tmp/php$1-fpm.log
 
-  [www]
-  listen = 127.0.0.1:9000
-  listen.owner = gitpod
-  listen.group = gitpod
+[www]
+listen = 127.0.0.1:9000
+listen.owner = gitpod
+listen.group = gitpod
 
-  pm = dynamic
-  pm.max_children = 5
-  pm.start_servers = 2
-  pm.min_spare_servers = 1
-  pm.max_spare_servers = 3" > "$2"
+pm = dynamic
+pm.max_children = 5
+pm.start_servers = 2
+pm.min_spare_servers = 1
+pm.max_spare_servers = 3" > "$2"
 }
 
 # Call functions from this script gracefully
