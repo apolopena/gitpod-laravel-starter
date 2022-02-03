93a6ba4a2e736b9a732d7fd4899aec6241b84047
apolopena <3060702+apolopena@users.noreply.github.com>
2022-02-03 08:42:36 -0800

    🐛 FIX: https://github.com/apolopena/gitpod-laravel-starter/issues/197

--- .gp/bash/open-preview.sh
+++ .gp/bash/open-preview.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # SPDX-License-Identifier: MIT
-# Copyright © 2021 Apolo Pena
+# Copyright © 2022 Apolo Pena
 #
 # open-preview.sh
 # Description:
@@ -11,22 +11,20 @@
 # Can take a port option to open a specific port, for example op -3005
 # In the case of using the port option, the path segment to open will be $2
 
-[[ $(pwd) != "$GITPOD_REPO_ROOT" ]] &&
-echo -e "\e[1;31mThis command can only be run from the project root of a gitpod workspace:\e[0m $GITPOD_REPO_ROOT" &&
-exit 1
-
 function main() {
+  local rp="$GITPOD_REPO_ROOT/.gp/"
   local path port
-  [[ $1 == '-h' || $1 == '--help' ]] && cat .gp/snippets/messages/op-help.txt && exit 0
+  [[ $1 == '-h' || $1 == '--help' ]] && cat "$rp/snippets/messages/op-help.txt" && exit 0
   if [[ $1 =~ ^-[1-9][0-9]+$ ]]; then
     port="${1:1}"
     path="/$2"
   else
     path="/$1"
-    port=$(bash .gp/bash/helpers.sh get_default_server_port)
+    port=$(bash "$rp/bash/helpers.sh" get_default_server_port)
   fi
-  if [[ $(bash .gp/bash/helpers.sh is_inited) == 0 ]]; then
-    . .gp/bash/spinner.sh &&
+  if [[ $(bash "$rp/bash/helpers.sh" is_inited) == 0 ]]; then
+    # shellcheck disable=SC1090,SC1091
+    . "$rp/bash/spinner.sh" &&
     start_spinner "Opening preview when system is ready..."
     gp sync-await gitpod-inited &&
     stop_spinner 0 &&
