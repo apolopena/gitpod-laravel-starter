32a08753abb26b9fafcb593cf09499ec2ad8d608
Rijwan Ansari <rijwan5g2022@gmail.com>
2022-07-12 07:26:51 +0000

    Initiation

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+/**
+ * BEGIN: Injected from .gp/snippets/laravel/routes/web/allow-mixed-web.snippet
+ */
+
+$url = config('app.url');
+resolve(\Illuminate\Routing\UrlGenerator::class)->forceRootUrl($url);
+resolve(\Illuminate\Routing\UrlGenerator::class)->forceScheme('https');
+
+ /**
+ * END: Injected from .gp/snippets/laravel/routes/web/allow-mixed-web.snippet
+ */
